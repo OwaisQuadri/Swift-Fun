44ebbad1f7038a375bc2fb0ad5f70251ae66757b
Owais Quadri <owaisquadri01@gmail.com>
2023-07-02 03:01:23 -0400

    tap button to change the colour and track the number of taps

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var changeColourButton: UIButton!
+    @IBOutlet weak var introLabel: UILabel!
+    var count = 0 {
+        didSet {
+            if count == 1 {
+                introLabel.text = "Colour has been changed \(count) time"
+            } else {
+                introLabel.text = "Colour has been changed \(count) times"
+            }
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        changeColourButton.setTitle("Change Colour", for: .normal)
+        count = 0
     }
-
+    @IBAction func changeIntroColour(_ sender: Any) {
+        introLabel.textColor = UIColor(red: .random(in: 0.0...1.0), green: .random(in: 0.0...1.0), blue: .random(in: 0.0...1.0), alpha: 1.0)
+        count += 1
+    }
+    
 
 }
 
